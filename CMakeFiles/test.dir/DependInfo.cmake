00762034d6479c65a22b832e355d9f5e1605ed27
Shawn Long <slong024@ucr.edu>
2021-01-16 21:00:15 -0800

    Iniial files

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/shawnlong/Downloads/cherno/main.cpp" "/Users/shawnlong/Downloads/cherno/CMakeFiles/test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_NO_GLU"
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "glfw/include"
+  "glew-cmake/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/shawnlong/Downloads/cherno/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/shawnlong/Downloads/cherno/glew-cmake/CMakeFiles/libglew_static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
